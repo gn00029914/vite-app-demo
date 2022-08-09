8d6bd8307b1510bf6351ca85c508de3a6499f1c6
gn00029914 <gn00029914@hotmail.com>
2022-08-10 01:29:46 +0800

    chore: modified the URL

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vite-app-demo/',
+  publicDir: 'public',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src')
