24f1dccc0d459a53a7bcbd03b73883783af80145
gn00029914 <gn00029914@hotmail.com>
2022-10-22 15:46:06 +0800

    chore: 調整網頁載入順序
    Signed-off-by: gn00029914 <gn00029914@hotmail.com>

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
   semi: false,
   singleQuote: true,
   trailingComma: 'none'
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,15 @@ module.exports = {
   darkMode: 'class',
   content: [
     './index.html',
-    './src/**/*.{js,ts,vue}',
-    './node_modules/flowbite/**/*.js',
-    './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}'
+    './src/**/*.{js,ts,vue}'
+    // './node_modules/flowbite/**/*.js'
+    // './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {}
   },
-  plugins: [require('flowbite/plugin')] //缺少官方支援 'flowbite/plugin' 的宣告 (.d.ts) 檔案
+  plugins: [
+    // 'flowbite/plugin'
+    // 'flowbite-vue'
+  ]
 }
