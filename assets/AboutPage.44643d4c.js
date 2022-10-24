4cd68e1071bbee5f056fada7c50bfa9672d2b3bc
gn00029914 <103336321+gn00029914@users.noreply.github.com>
2022-10-24 11:28:26 +0000

    Deploying to gh-pages from @ gn00029914/vite-app-demo@ff59b6593f2da4a56e9ffa71e5236a0076386a25 🚀

---
+++ assets/AboutPage.44643d4c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as _,c as t}from"./index.d3e76a92.js";const o={};function c(a,n){return _(),t("div",null,"About")}const r=e(o,[["render",c],["__scopeId","data-v-2a7315fb"]]);export{r as default};
--- assets/HomePage.fdf61b31.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c}from"./index.6e123be5.js";const o={};function t(n,a){return _(),c("div",null,"Home")}const r=e(o,[["render",t],["__scopeId","data-v-35c462de"]]);export{r as default};
