e78206f2c318e1a93aaee574229493fc6eaa7d08
gn00029914 <103336321+gn00029914@users.noreply.github.com>
2022-11-09 17:02:38 +0000

    Deploying to gh-pages from @ gn00029914/vite-app-demo@8b0cfcb60c4964e79d8a7e77022469d21e8b1c37 🚀

--- assets/AboutPage.6a6fec26.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as _,c as t}from"./index.5116920c.js";const o={};function c(a,n){return _(),t("div",null,"About")}const r=e(o,[["render",c],["__scopeId","data-v-2a7315fb"]]);export{r as default};
-//# sourceMappingURL=AboutPage.6a6fec26.js.map
