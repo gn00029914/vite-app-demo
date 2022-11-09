49fb00ac5be2a58765ad65ff758b85d6c0c9b151
gn00029914 <gn00029914@hotmail.com>
2022-11-10 00:56:24 +0800

    chore: migrate the husky 4 to husky 8 for DX
    Signed-off-by: gn00029914 <gn00029914@hotmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,43 +2,47 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2017: true,
+    es2022: true,
     node: true
   },
   extends: [
-    'eslint:recommended', // this maybe causes errors in defineEmits<{}>() ???
-    'plugin:@typescript-eslint/recommended',
+    // 'eslint:recommended', // this maybe causes errors in defineEmits<{}>() ???
+    // 'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
-    'plugin:prettier/recommended',
-    'plugin:tailwindcss/recommended' // 規則較prettier不嚴謹
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['*.cjs'],
-  /* globals: {
-      defineEmits: 'readonly',
-      defineProps: 'readonly',
-    }, */
+  // /* globals: {
+  //     defineEmits: 'readonly',
+  //     defineProps: 'readonly',
+  //   }, */
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
-    ecmaVersion: 2020,
+    ecmaVersion: 'latest',
     ecmaFeatures: {
       jsx: true
+    },
+    vueFeatures: {
+      filter: true,
+      interpolationAsNonHTML: false,
     }
     // project: ['tsconfig.json', 'tsconfig.node.json'],
   },
   // plugins: ['@typescript-eslint'], // might not be needed
-  plugins: ['@typescript-eslint', 'prettier', 'tailwindcss'],
+  plugins: ['@typescript-eslint', 'vue', 'tailwindcss', 'prettier'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    'tailwindcss/classnames-order': 'warn',
-    'tailwindcss/no-custom-classname': 'warn',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto'
-      }
-    ]
+    // '@typescript-eslint/no-explicit-any': 'warn',
+    // '@typescript-eslint/consistent-type-imports': 'warn',
+    // 'tailwindcss/classnames-order': 'warn',
+    // 'tailwindcss/no-custom-classname': 'warn',
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     endOfLine: 'auto'
+    //   }
+    // ]
   }
 }
