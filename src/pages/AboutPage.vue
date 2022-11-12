6619be96c11a5758717136822eceab6d339f731d
gn00029914 <gn00029914@hotmail.com>
2022-11-12 21:19:21 +0800

    refactor: 調整專案目錄結構

--- src/pages/AboutPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>About</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped>
-/* todo */
-</style>
--- src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // @vue/component
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
