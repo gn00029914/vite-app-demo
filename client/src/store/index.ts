d007641817168733eb54af30f5cdae375bf7ea5e
gn00029914 <gn00029914@hotmail.com>
2023-09-25 22:16:53 +0800

    feat: 新增表單驗證支援
    Signed-off-by: gn00029914 <gn00029914@hotmail.com>

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,5 +1,7 @@
 import { defineStore } from 'pinia'
 import { Names } from './store-name'
+import { useForm } from 'vee-validate'
+import * as Yup from 'yup'
 
 export const useTestStore = defineStore(Names.Test, {
     state: () => {
@@ -9,4 +11,42 @@ export const useTestStore = defineStore(Names.Test, {
     },
     getters: {},
     actions: {}
+})
+
+const schema = Yup.object({
+    firstName: Yup.string().required(),
+    lastName: Yup.string().required(),
+    userName: Yup.string().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string().min(6).required(),
+    phoneNumber: Yup.string().required()
+})
+
+export const useSigninStore = defineStore('signin', () => {
+    const { errors, defineInputBinds, handleSubmit } = useForm({
+        validationSchema: schema
+    })
+
+    const firstName = defineInputBinds('firstName')
+    const lastName = defineInputBinds('lastName')
+    const userName = defineInputBinds('userName')
+    const email = defineInputBinds('email')
+    const password = defineInputBinds('password')
+    const phoneNumber = defineInputBinds('phoneNumber')
+
+    const signin = handleSubmit((values: unknown) => {
+        // send values to API
+        console.log('Submit', JSON.stringify(values, null, 2))
+    })
+
+    return {
+        errors,
+        firstName,
+        lastName,
+        userName,
+        email,
+        password,
+        phoneNumber,
+        signin
+    }
 })
